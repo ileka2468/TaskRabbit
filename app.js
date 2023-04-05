48e47a74c6b2341d148c08f230167eadc1920fc2
William Ileka <william@Williams-MacBook-Air.local>
2023-04-05 08:16:46 -0500

    added layout files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var partials = require('express-partials');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contact', contactRouter);
 app.use('/about', aboutRouter)
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
