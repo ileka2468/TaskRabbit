828ba5fa614ad88b1332cef3a533b1ddfe1b386f
William <ileka2468@gmail.com>
2023-05-17 17:04:23 -0500

    update

--- routes/reports.js
+++ routes/reports.js
@@ -17,12 +17,35 @@ router.get('/customers', function (req, res, next) {
         res.render('subreports/customerreport', {records: results})
     })
 
+})
 
+router.get('/sales', function (req, res, next) {
+
+    let query = "SELECT order_id, buyer_id, seller_id, service_id, fulfilled, created_at, priceAtSale FROM orders"
+    db.query(query, (err, results) =>{
+        if (err){
+            console.log(err)
+            res.render('error')
+        }
+        console.log(results)
+        res.render('subreports/salesreport', {records: results})
+    })
 
 })
 
+router.get('/products', function (req, res, next) {
 
+    let query = "SELECT service_id, title, description, price, delivery_time, seller_id, gig_category_id, homepage FROM gigs"
+    db.query(query, (err, results) =>{
+        if (err){
+            console.log(err)
+            res.render('error')
+        }
+        console.log(results)
+        res.render('subreports/productsreport', {records: results})
+    })
 
+})
 
 
 module.exports = router
