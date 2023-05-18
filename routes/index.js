0001bad456204b871c2ff9ccf1c2509de7b2a341
William <ileka2468@gmail.com>
2023-05-17 23:33:48 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  let query =
-      'SELECT service_id, title, description, price, delivery_time, homepage FROM gigs WHERE homepage = true'
+
+
+router.get('/', function(req, res, next) {
+  let query = "SELECT service_id, title, description, price, delivery_time, gigimage FROM gigs WHERE homepage = true";
   // execute query
   db.query(query, (err, result) => {
     if (err) {
-      console.log(err)
-      res.render('error')
+      console.log(err);
+      res.render('error');
     }
-    res.render('index', { allrecs: result })
-  })
-})
+
+    let query = "SELECT promotion_id, promotitle, promoimage FROM promotions WHERE startdate <= CURRENT_DATE() and enddate >= CURRENT_DATE() ";
+    // execute query
+    db.query(query, (err, result2) => {
+      if (err) {
+        console.log(err);
+        res.render('error');
+      }
+      console.log("Promos is " + result2[0].promotitle)
+      res.render('index', {allrecs: result , promolist: result2});
+    });
+  });
+
+
+});
+
+
 module.exports = router;
+
